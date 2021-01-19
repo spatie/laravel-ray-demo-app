0694c75b49138a21883ba7d1f393c465f59f932c
freek <freek@spatie.be>
2021-01-19 16:53:18 +0100

    wip

---
+++ app/Console/Commands/SpecialValuesCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SpecialValuesCommand extends Command
+{
+    protected $signature = 'special-values';
+
+    public function handle()
+    {
+        ray()->newScreen();
+        ray('null', null);
+        ray('false', false);
+        ray('true', true);
+    }
+}
