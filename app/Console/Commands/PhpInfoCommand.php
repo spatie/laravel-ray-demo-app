b8655df4b7a145037a8bc6f116a8b6a03f0694f1
freek <freek@spatie.be>
2021-01-22 19:40:19 +0100

    wip

---
+++ app/Console/Commands/PhpInfoCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PhpInfoCommand extends Command
+{
+    protected $signature = 'php-info';
+
+    public function handle()
+    {
+        ray()->newScreen();
+
+        ray()->phpinfo();
+
+        ray()->phpinfo('default_mimetype');
+    }
+}
