7b7c16e095ee1aec998b5b326c01ddfafd0b5ab5
Niels <10651054+Nielsvanpach@users.noreply.github.com>
2021-10-07 12:11:12 +0200

    add command to test if global functions can be send to Ray

---
+++ app/Console/Commands/GlobalFunctionCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GlobalFunctionCommand extends Command
+{
+    protected $signature = 'global-function';
+
+    public function handle()
+    {
+        ray()->newScreen('Test a global function name');
+
+        ray('config');
+        ray('app');
+        ray('array_map');
+        ray('is_callable');
+
+        return;
+    }
+}
