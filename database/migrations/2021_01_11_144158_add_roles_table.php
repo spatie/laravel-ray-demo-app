8b11c8e18d56df7e8beb30eb68b0394510a00abf
Shift <shift@laravelshift.com>
2022-02-15 14:32:18 +0000

    Adopt anonymous migrations

--- database/migrations/2021_01_11_144158_add_roles_table.php
+++ database/migrations/2021_01_11_144158_add_roles_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRolesTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class AddRolesTable extends Migration
             $table->unsignedBigInteger('role_id')->nullable();
         });
     }
-}
+};
