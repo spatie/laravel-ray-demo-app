f7f94ef6733b883ee801087472e1d0ebba6ec7b7
Freek Van der Herten <freek@spatie.be>
2022-08-25 09:30:40 +0200

    wip

---
+++ app/Console/Commands/NewScreenCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\TestEvent;
+use App\Mail\TestMailable;
+use App\Models\User;
+use App\Services\ClassWithRayCall;
+use Illuminate\Console\Command;
+use Spatie\Ray\Ray;
+
+class NewScreenCommand extends Command
+{
+    protected $signature = 'new-screen';
+
+    public function handle()
+    {
+        ray()->newScreen('This is a new screen');
+
+        ray('content of screen');
+    }
+}
