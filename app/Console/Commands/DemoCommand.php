2ffeff7d0ac903029c274b4fefa2f9adb28ff592
Freek Van der Herten <freek@spatie.be>
2022-09-01 18:33:00 +0200

    wip

--- app/Console/Commands/DemoCommand.php
+++ app/Console/Commands/DemoCommand.php
@@ -10,15 +10,6 @@ class DemoCommand extends Command
 
     public function handle()
     {
-        ray()->clearScreen();
 
-        ray('hi everybody');
-        ray('hi everybody')->label('THIS');
-        ray('hi everybody')->large();
-        ray('hi everybody')->red();
-        ray('hi everybody')->green();
-        ray('hi everybody')->blue();
-
-        ray('something else', ['a' => 1]);
     }
 }
